dc347f9f691f2d832b67c405b20d226f0173595d
Joshwa Fugett <jfugett@thotjs.io>
2014-06-23 11:15:48 -0700

    chore(temp): Temporary commit for moving between machines

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
 'use strict';
 
-require('thot-cwd');
-
-var main = function main(cb) {
-
-  var Module = require('module').Module;
-  var _load = Module._load;
-
-  var thotRequire = function thotRequire(request, parent, isMain) {
-    var timeIn = process.hrtime();
-    var value = _load(request, parent, isMain);
-    if(cb){
-      var diff = process.hrtime(timeIn);
-      cb(diff[0] * 1e9 + diff[1]);
-    }
-    return value;
-  };
-
-  Module._load = thotRequire;
-};
-
-module.exports = main;
